bba6d8973c8938a50944577ce79c17f90542cbf8
evertcolombia <evertescalante@gmail.com>
2021-08-20 21:34:14 -0500

    forma│[9589] Hacked 8)nnnAAA! (0x5595bb126260) t for the read write on heap script

--- 0x03-proc_filesystem/read_write_heap.py
+++ 0x03-proc_filesystem/read_write_heap.py
@@ -14,9 +14,9 @@ Where:
 
 import sys
 
-def default_exit():
-    print("bad usage")
-    exit(1)
+def default_exit(msg):
+    print("\n{}".format(msg))
+    sys.exit(1)
 
 def validate_str(to_validate):
     if to_validate == "":
@@ -24,88 +24,82 @@ def validate_str(to_validate):
     return to_validate
 
 if len(sys.argv) != 4:
-    default_exit()
+    default_exit("Args must be 3")
+
 try:
     pid = int(sys.argv[1]) # pid of the process
     if pid <= 0:
-        print("pid must be greather than 0")
-        exit(1)
-except ValueError:
-    default_exit()
+        default_exit("[err] pid must be greather than 0")
+except ValueError as e:
+    default_exit(e)
 
 # search and replace strings
 search_str = validate_str(sys.argv[2]) 
 replace_str = validate_str(sys.argv[3])
 
+# path's to maps and mem files for current pid
 maps_path = "/proc/{}/maps".format(pid)
-print("maps path: {}".format(maps_path))
 mem_path = "/proc/{}/mem".format(pid)
-print("mem path: {}".format(mem_path))
+print("\npath's to use: {} - {}\n".format(maps_path, mem_path))
 
 try:
     maps_lines = open(maps_path, 'r')
 except IOError as e:
-    maps_lines.close()
-    print(e)
-    exit()
+    default_exit(e)
 
+# search the heap information
 heap_line = list()
-for line in maps_lines: # search heap information
+print("Searching heap on maps file")
+for line in maps_lines:
     line = line.split()
     if line[-1][:] == "[heap]":
         heap_line = line
-        print("heap found!")
+        print("Heap was found!")
         break
-
 maps_lines.close()
+
 # parse the heap line
 perms = heap_line[1]
 if 'w' not in perms and 'r' not in perms:
-    print("does not have read write perms")
+    default_exit("[err] does not have read write perms")
     exit(1)
 
 addr = heap_line[0].split("-")
 if len(addr) != 2:
-    print("wrong address format")
-    exit(1)
+    default_exit("[err] wrong address format")
 addr_start = int(addr[0], 16)
 addr_end = int(addr[1], 16)
 
 offset = heap_line[2]
 dev = heap_line[3]
 inode = heap_line[4]
-pathname = heap_line[-1][:]
-
-print("{}\n{}\n{}\n{}\n{}\n{}".format(
-    '-'.join(addr), perms, offset, dev, inode, pathname))
+path = heap_line[-1][:]
+print("\tPath: {}\n\tAdress: {}\n\tPerms: {}\n\tOffset: {} \
+        \n\tDev: {}\n\tInode: {}"
+        .format(path, '-'.join(addr), perms, offset, dev, inode))
 print("start: {}\nend: {}".format(addr_start, addr_end))
 
 # open and read mem file
 try:
     mem_lines = open(mem_path, 'rb+')
 except IOError as e:
-    mem_lines.close()
-    print(e)
-    exit(1)
+    default_exit(e)
 
 # search the heap on virtual memory
 mem_lines.seek(addr_start)
 heap = mem_lines.read(addr_end - addr_start)
-
-#print(heap)
 
 # search the string in the heap
 try:
     i = heap.index(bytes(search_str, "ASCII"))
 except:
     mem_lines.close()
-    print("cant find {}".format(search_str))
-    exit(1)
-print("found {} at {:x}".format(search_str, i))
+    default_exit("[err] cant find {}".format(search_str))
+print("Found search string {} at {:x}".format(search_str, i))
 
 # write the new string in the heap
-print("write {} at {:x}".format(replace_str, addr_start + i))
 mem_lines.seek(addr_start + i)
 mem_lines.write(bytes(replace_str, "ASCII"))
-
 mem_lines.close()
+print("write {} at {:x}".format(replace_str, addr_start + i))
+print("well done!")
