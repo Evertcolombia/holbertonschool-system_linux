2f4529c137d9b5a907cc311f25a7f5e1fdf459a6
evertcolombia <evertescalante@gmail.com>
2021-09-16 18:59:23 -0500

    add comments

--- 0x05-libasm/0-strlen.asm
+++ 0x05-libasm/0-strlen.asm
@@ -13,15 +13,16 @@ global asm_strlen	; Export our 'asm_strlen' function
 	; Return: length of the str
 
 asm_strlen:
-	mov eax, 0
-	mov cl, [edi]
-	while: cmp cl, 0
-		jz return		
-		inc eax
-		inc edi
-		mov cl, [edi]
-		cmp cl, 0
-		jnz while
+	mov eax, 0		; init the counter in eax
+	mov cl, [edi]		; get the byte value pointed by edi
+	
+	while: cmp cl, 0		; compare the value to \0
+		jz return		; if null return
+		inc eax			; Increment the counter
+		inc edi			; increment the str pointer
+		mov cl, [edi]		; get the byte value
+		cmp cl, 0		; compare the value to \0
+		jnz while		; if not zero jump to while again
 
 	return:
 	ret
--- 0x05-libasm/1-strcmp.asm
+++ 0x05-libasm/1-strcmp.asm
@@ -13,41 +13,40 @@ global asm_strcmp		; Export our 'asm_strcmp; function
 	; Return: value
 
 asm_strcmp:
-	mov bl, [edi]
-	mov cl, [esi]
+	mov bl, [edi]		; get the byte value pointed by edi
+	mov cl, [esi]		; get the byte value pointed by esi
 	
-	sub bl, cl
-	cmp bl, 0
-	jne return
+	sub bl, cl		; subtract cl to bl and get the rest in bl
+	cmp bl, 0		; compare bl to zero
+	jnz return		; if not zero return
 
 	while:	
-		inc edi
-		inc esi
-		mov bl, [edi]
-		mov cl, [esi]
+		inc edi			; increment the str pointer @s1
+		inc esi			; increment the str pointer @s2
+		mov bl, [edi]		; get the byte value pointed by @s1
+		mov cl, [esi]		; get the byte value pointed by @s2
 							
-		sub bl, cl
-		cmp bl, 0
-		jne return
+		sub bl, cl		; subtract cl to bl
+		cmp bl, 0		; compare the result with 0
+		jnz return		; if not zero return
 		
-		cmp cl, 0
-		jne while
+		cmp cl, 0		; compare the value on cl
+		jnz while		; if not zero jump while
 
 	return:
-	;mov al, bl
-	cmp bl, 0
-	je return_zero
-	jg return_signed
-	jl return_unsigned
+	cmp bl, 0			; get the value to return
+	je return_zero			; if zero
+	jg return_signed		; greather than 0
+	jl return_unsigned		; less than 0
 
 return_zero:
-	mov eax, 0
+	mov eax, 0		; return 0
 	ret	
 
 return_signed:
-	mov eax, 1
+	mov eax, 1		; return 1
 	ret
 
 return_unsigned:
-	mov eax, -1
+	mov eax, -1		; return -1
 	ret
