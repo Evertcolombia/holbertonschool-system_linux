edf0f2668abba4850d43b19dbb32680d79592e87
evertcolombia <evertescalante@gmail.com>
2021-09-26 13:15:47 -0500

    add habdler signal for task 0

---
+++ 0x06-signals/signals.h
@@ -0,0 +1,13 @@
+#ifndef _SIGNALS_H_
+#define _SIGNALS_H_
+
+
+#include <signal.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int handle_signal(void);
+
+
+#endif /* _SIGNALS_H_ */
