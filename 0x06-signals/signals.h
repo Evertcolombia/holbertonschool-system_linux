bdafbb4b179ad2dc3534a47d50022367a7174c6b
evertcolombia <evertescalante@gmail.com>
2021-09-26 23:50:51 -0500

    hotfix

--- 0x06-signals/signals.h
+++ 0x06-signals/signals.h
@@ -6,8 +6,19 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
 
 int handle_signal(void);
 void (*current_handler_signal(void))(int);
+int handle_sigaction(void);
+void (*current_handler_sigaction(void))(int);
+int trace_signal_sender(void);
+int pid_exist(pid_t pid);
+void all_in_one(void);
+int sigset_init(sigset_t *set, int *signals);
+int signals_block(int *signals);
+int signals_unblock(int *signals);
+int handle_pending(void (*handler)(int));
 
 #endif /* _SIGNALS_H_ */
