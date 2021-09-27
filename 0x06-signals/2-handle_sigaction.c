acfd5a55bf62096a5f0a35e5a6731b7761d00c01
evertcolombia <evertescalante@gmail.com>
2021-09-26 23:50:10 -0500

    hotfix

--- 0x06-signals/2-handle_sigaction.c
+++ 0x06-signals/2-handle_sigaction.c
@@ -1,11 +1,23 @@
 #include "signals.h"
 
+/**
+ * print_gotcha - print gotcha
+ * @signal: signal
+ *
+ * Return: None
+ */
 void print_gotcha(int signal)
 {
 	printf("Gotcha! [%d]\n", signal);
 	fflush(stdout);
 }
 
+/**
+ * handle_sigaction - hanlde sig action
+ * @void: void
+ *
+ * Return: None
+ */
 int handle_sigaction(void)
 {
 	struct sigaction act;
