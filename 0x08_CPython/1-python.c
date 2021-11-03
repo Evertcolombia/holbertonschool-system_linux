ffdd0575492e743a3c1a15db65203337a1996578
evertcolombia <evertescalante@gmail.com>
2021-11-03 02:23:23 +0000

    hotfix

--- 0x08_CPython/1-python.c
+++ 0x08_CPython/1-python.c
@@ -8,7 +8,7 @@ void print_python_list(PyObject *p)
 	ptr = (PyListObject *)p;
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %d\n", (int)(PyList_Size(p)));
-	printf("[*] Allocated = %d\n", (int)ptr->allocated);
+	/*printf("[*] Allocated = %d\n", (int)ptr->allocated);*/
 
 	for (count = 0; count < (int)(PyList_Size(p)); count++)
 	{
