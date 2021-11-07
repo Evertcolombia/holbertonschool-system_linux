f2c71f45318818b3f348aedef2e2bbc0efc6c374
evertcolombia <evertescalante@gmail.com>
2021-11-07 17:30:26 +0000

    upload tasks

---
+++ 0x08_CPython/4-python.c
@@ -0,0 +1,33 @@
+#include <Python.h>
+
+void print_python_string(PyObject *p)
+{
+	int tamano = 0;
+	char *buffer;
+
+	printf("[.] string object info\n");
+	fflush(stdout);
+	if (PyUnicode_Check(p) && PyUnicode_READY(p) == 0)
+	{
+		buffer = (char *)PyUnicode_AsUTF8(p);
+		tamano = (int) PyUnicode_GET_LENGTH(p);
+		if (PyUnicode_IS_COMPACT_ASCII(p))
+		{
+			printf("  type: compact ascii\n");
+		}
+		else
+		{
+			printf("  type: compact unicode object\n");
+		}
+		fflush(stdout);
+		printf("  length: %d\n", tamano);
+		fflush(stdout);
+		printf("  value: %s\n", buffer);
+		fflush(stdout);
+	}
+	else
+	{
+		printf("  [ERROR] Invalid String Object\n");
+		fflush(stdout);
+	}
+}
